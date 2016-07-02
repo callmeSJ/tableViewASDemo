994509890b895fd5ccf5564a0ff1f0f2836e2d4c
SJ <shijie7575@foxmail.com>
2016-07-02 22:38:32 +0800

    Initial Commit

---
+++ tableViewS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewS
+//
+//  Created by SJ on 16/7/2.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
